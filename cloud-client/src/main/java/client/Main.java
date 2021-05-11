1eb450d6933c025977f31c8a6cf2247a48b53318
igorkunicyn <igor_kunicyn@inbox.ru>
2021-05-11 20:53:17 +0300

    Initial commit

---
+++ cloud-client/src/main/java/client/Main.java
@@ -0,0 +1,24 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
+        Parent parent = loader.load();
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.setTitle("Cloud Storage");
+        primaryStage.setResizable(true);
+        Controller controller = loader.getController();
+        primaryStage.setOnCloseRequest((event) -> controller.shutdown());
+        primaryStage.show();
+    }
+
+}
